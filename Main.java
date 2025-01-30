b7d40b25b83c353eaa4ea34431bc24110d35c8cf
Safmica <safmikasa@gmail.com>
2025-01-30 20:30:21 +0700

    feat: enkapsulasi

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    //Enkapsulasi
+    public class Mahasiswa {
+        public String name;
+        private int IPK;
+      
+        public int getName() {
+          return IPK;
+        }
+      
+        public void setIPK(int newIPK) {
+          this.IPK = newIPK;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
